7ec378ddb8b2e2d296e630d577629fd0f39e5818
augustine-ok <augustine.opoku-kesse@gmail.com>
2023-04-11 18:10:02 +0000

    Stable: Handled the play button hover

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import playIcon from '../assets/images/icon-play.svg'
+//import playIcon from '../assets/images/icon-play.svg'
 import '../assets/styles/AudioPlayer.scss';
 
 interface AudioPlayerProps {
@@ -14,8 +14,15 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioUrl }) => {
 
   return (
     <button onClick={handleClick} className='audioPlayButton'>
-      <img src={playIcon} alt="Play icon" />
-      {/* <i className="fas fa-volume-up"></i> Play Audio */}
+      {/* <img src={playIcon} alt="Play icon" /> */}
+      <div className="play-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 75 75">
+          <g fill="#A445ED" fill-rule="evenodd">
+            <circle cx="37.5" cy="37.5" r="37.5" opacity=".25"/>
+            <path d="M29 27v21l21-10.5z"/>
+          </g>
+        </svg>
+      </div>
       <audio src={audioUrl} />
     </button>
   );
